ef51e82c0827412a52da04bd73161d519ed1a25e
hayashi.tomohiro <hayashi.tomohiro@digitalvalue.co.jp>
2024-11-08 19:33:52 +0900

    add Domain, Infra projects

---
+++ CleanArchitecture-sample/Service/Deposit/Deposit-Infrastructure/src/main/java/com/sample/depositinfrastructure/DepositInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.sample.depositinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositInfrastructureApplication.class, args);
+	}
+
+}
