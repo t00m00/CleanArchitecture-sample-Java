ca1587a35d427f39f9e7a039881e53eed5b24731
hayashi.tomohiro <hayashi.tomohiro@digitalvalue.co.jp>
2024-11-08 18:01:15 +0900

    add Application project

---
+++ CleanArchitecture-sample/Service/Deposit/Deposit-Application/src/main/java/com/sample/depositapplication/DepositApplication.java
@@ -0,0 +1,13 @@
+package com.sample.depositapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositApplication.class, args);
+	}
+
+}
