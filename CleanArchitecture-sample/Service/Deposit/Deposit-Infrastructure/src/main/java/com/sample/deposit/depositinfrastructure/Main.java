369faadbb751ba819ef55e821b0b76b18ca17925
hayashi.tomohiro <hayashi.tomohiro@digitalvalue.co.jp>
2024-11-11 15:24:17 +0900

    delete unnecessary Main file

--- CleanArchitecture-sample/Service/Deposit/Deposit-Infrastructure/src/main/java/com/sample/deposit/depositinfrastructure/Main.java
+++
@@ -1,7 +0,0 @@
-package com.sample.deposit.depositinfrastructure;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- CleanArchitecture-sample/Shared/Shared-Contracts/src/main/java/com/sample/shared/sharedcontract/Main.java
+++
@@ -1,7 +0,0 @@
-package com.sample.shared.sharedcontract;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
